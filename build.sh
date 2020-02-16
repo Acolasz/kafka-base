3167313efc66aa8ca6bddf78efbafe46080883f3
Olivér <Ocsko.oliver13@gmail.com>
2020-02-16 15:46:20 +0100

    modify bash file

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
+scala_version=2.12
+kafka_version=2.4.0
+
 if [ $# -gt 0 ]; then
     echo "scala_version=$1"
+	scala_version=$1
     echo "kafka_version=$2"
-	docker.exe build -t acolasz/myzookeeper:$2 --build-arg scala_version=$1 --build-arg kafka_version=$2 .
-
-else
-    echo "Your command line contains no arguments"
+	kafka_version=$2
 fi
+    
+docker.exe build -t acolasz/kafka-base:$kafka_version --build-arg scala_version=$scala_version --build-arg kafka_version=$kafka_version .
